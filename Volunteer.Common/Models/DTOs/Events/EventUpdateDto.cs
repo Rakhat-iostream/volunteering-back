a40f4a7a57da37a85c95a01706bf4585a040b295
Rakhat Toleu <raha0264@gmail.com>
2022-05-28 20:48:50 +0600

    EventUpdate objects

--- Volunteer.Common/Models/DTOs/Events/EventUpdateDto.cs
+++ Volunteer.Common/Models/DTOs/Events/EventUpdateDto.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Volunteer.Common.Models.Domain;
+using Volunteer.Common.Models.Domain.Enum;
 
 namespace Volunteer.Common.Models.DTOs.Events
 {
@@ -12,6 +9,9 @@ namespace Volunteer.Common.Models.DTOs.Events
         public int EventId { get; set; }
         public string EventName { get; set; }
         public string Description { get; set; }
+        public Region Region { get; set; }
+        public string? Location { get; set; }
+        public string? Image { get; set; }
         public DateTime Deadline { get; set; }
         public DateTime EndDate { get; set; }
         public VolunteeringCategories VolunteeringCategory { get; set; }
